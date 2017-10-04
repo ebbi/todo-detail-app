7faed18d9fc03826ed89048a72750bb5765903a9
Ebbi <ebbi40@gmail.com>
2017-10-04 03:01:08 +0100

    added landscpe view and Logger

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -3,6 +3,7 @@ package com.example.todo;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,13 +13,13 @@ public class TodoActivity extends AppCompatActivity {
     private String[] mTodos;
     private int mTodoIndex = 0;
 
-    /*
-       In case of state change, due to rotating the phone
-       store the mTodoIndex to display the same mTodos element post state change
-       N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
-       the alternative is to abstract view data to a ViewModel which can be in scope in all
-       Activity states and more suitable for larger amounts of data
-    */
+    public static final String TAG = "TodoActivity";
+
+    // In case of state change, due to rotating the phone
+    // store the mTodoIndex to display the same mTodos element post state change
+    // N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
+    // the alternative is to abstract view data to a ViewModel which can be in scope in all
+    // Activity states and more suitable for larger amounts of data
     private static final String TODO_INDEX = "todo_index";
     // override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key
     @Override
@@ -33,6 +34,8 @@ public class TodoActivity extends AppCompatActivity {
         // call the super class onCreate to complete the creation of Activity
         // like the view hierarchy
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, " **** Just to say the PC is in onCreate!");
 
         // set the user interface layout for this Activity
         // the layout file is defined in the project res/layout/activity_todo.xml file
