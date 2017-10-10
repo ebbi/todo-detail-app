0b3dd9932e6e3b0367e42b60a4589fc29b35bc08
Ebbi <ebbi40@gmail.com>
2017-10-10 16:58:52 +0100

    intent extra data and style Theme

--- app/src/main/java/com/example/todo/TodoActivity.java
+++ app/src/main/java/com/example/todo/TodoActivity.java
@@ -3,18 +3,24 @@ package com.example.todo;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TodoActivity extends AppCompatActivity {
 
+    private static final int IS_SUCCESS = 0;
     private String[] mTodos;
     private int mTodoIndex = 0;
 
     public static final String TAG = "TodoActivity";
+
+    // name, value pair to be returned in an intent
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
 
     // In case of state change, due to rotating the phone
     // store the mTodoIndex to display the same mTodos element post state change
@@ -49,36 +55,91 @@ public class TodoActivity extends AppCompatActivity {
         }
 
         // initialize member TextView so we can manipulate it later
-        final TextView TodoTextView;
-        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
 
-        // read the todo array from res/values/strings.xml
+        setTextViewComplete("");
+
+        /*
+        TODO: Refactor to data layer
+         */
         Resources res = getResources();
         mTodos = res.getStringArray(R.array.todo);
-        // display the first task from mTodo array in the TodoTextView
-        TodoTextView.setText(mTodos[mTodoIndex]);
+        // display the first task from mTodo array in the textViewTodo
+        textViewTodo.setText(mTodos[mTodoIndex]);
 
         Button buttonNext = (Button) findViewById(R.id.buttonNext);
-
-        // OnClick listener for the  Next button
         buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 mTodoIndex = (mTodoIndex + 1) % mTodos.length;
-                TodoTextView.setText(mTodos[mTodoIndex]);
+                textViewTodo.setText(mTodos[mTodoIndex]);
+                setTextViewComplete("");
             }
         });
 
         Button buttonTodoDetail = (Button) findViewById(R.id.buttonTodoDetail);
-
         buttonTodoDetail.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
+                // Note, the child class being called has a static method determining the parameter
+                // to be passed to it in the intent object
                 Intent intent = TodoDetailActivity.newIntent(TodoActivity.this, mTodoIndex);
-                startActivity(intent);
+
+                 // second param requestCode identifies the call as there could be many "intents"
+                startActivityForResult(intent, IS_SUCCESS);
+                // The result will come back through
+                // onActivityResult(requestCode, resultCode, Intent) method
+
             }
         });
 
+    }
+
+    /*
+        requestCode is the integer request code originally supplied to startActivityForResult
+        resultCode is the integer result code returned by the child activity through its setResult()
+        intent date attached with intent "extras"
+    */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS ){
+            if (intent != null) {
+                // data in intent from child activity
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void updateTodoComplete(boolean is_todo_complete) {
+
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
+
+        if (is_todo_complete) {
+            textViewTodo.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+
+            setTextViewComplete("\u2713");
+        }
+
+    }
+
+    private void setTextViewComplete( String message ){
+        final TextView textViewComplete;
+        textViewComplete = (TextView) findViewById(R.id.textViewComplete);
+
+        textViewComplete.setText(message);
     }
 
 }
--- app/src/main/java/com/example/todo/TodoDetailActivity.java
+++ app/src/main/java/com/example/todo/TodoDetailActivity.java
@@ -2,32 +2,44 @@ package com.example.todo;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TodoDetailActivity extends AppCompatActivity {
 
     private int mTodoIndex;
 
+    // Any calling activity would call this static method and pass the necessary
+    // key, value data pair in an intent object.
     public static Intent newIntent(Context packageContext, int todoIndex){
         Intent intent = new Intent(packageContext, TodoDetailActivity.class);
         intent.putExtra(TODO_INDEX,todoIndex);
         return intent;
     }
 
+    // name, value pair to be returned in an intent
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
+
     // In case of state change, due to rotating the phone
     // store the mTodoIndex to display the same mTodos element post state change
     // N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
     // the alternative is to abstract view data to a ViewModel which can be in scope in all
     // Activity states and more suitable for larger amounts of data
-    private static final String TODO_INDEX = "com.example.todo_index";
+    private static final String TODO_INDEX = "com.example.todoIndex";
     // override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
         savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +54,63 @@ public class TodoDetailActivity extends AppCompatActivity {
         TextView TodoDetailTextView;
         TodoDetailTextView = (TextView) findViewById(R.id.textViewTodoDetail);
 
+        // get the intent extra int for the todos index
         int mTodoIndex = getIntent().getIntExtra(TODO_INDEX, 0);
-        TodoDetailTextView.setText("Intent passed the index " + mTodoIndex);
+        updateTextViewTodoDetail(mTodoIndex);
+
+        CheckBox checkboxIsComplete = (CheckBox)findViewById(R.id.checkBoxIsComplete);
+        // Register the onClick listener with the generic implementation mTodoListener
+        checkboxIsComplete.setOnClickListener(mTodoListener);
+
     }
+
+
+    private void updateTextViewTodoDetail(int todoIndex) {
+
+        final TextView textViewTodoDetail;
+        textViewTodoDetail = (TextView) findViewById(R.id.textViewTodoDetail);
+
+        /*
+            TODO: refactor to a data layer
+        */
+        Resources res = getResources();
+        String[] todoDetails = res.getStringArray(R.array.todo_detail);
+        // display the first task from mTodo array in the TodoTextView
+        textViewTodoDetail.setText(todoDetails[todoIndex]);
+
+    }
+
+    // Create an anonymous implementation of OnClickListener for all clickable view objects
+    private OnClickListener mTodoListener = new OnClickListener() {
+        public void onClick(View v) {
+            // get the clicked object and do something
+            switch (v.getId() /*to get clicked view object id**/) {
+                case R.id.checkBoxIsComplete:
+                    CheckBox checkboxIsComplete = (CheckBox)findViewById(R.id.checkBoxIsComplete);
+                    setIsComplete(checkboxIsComplete.isChecked());
+                    finish();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+    private void setIsComplete(boolean isChecked) {
+
+        // celebrate with a static Toast!
+        if(isChecked){
+            Toast.makeText(TodoDetailActivity.this,
+                    "Hurray, it's done!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(TodoDetailActivity.this,
+                    "There is always tomorrow!", Toast.LENGTH_LONG).show();
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra(IS_TODO_COMPLETE, isChecked);
+        setResult(RESULT_OK, intent);
+    }
+
 }
+
